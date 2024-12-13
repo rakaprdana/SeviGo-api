98d062d26db09bd69ec47aba5c04818ce1f7a631
rakaprdana <raka.pradana1103@gmail.com>
2024-12-13 13:48:26 +0700

    add: allow CORS for frontend

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -1,6 +1,6 @@
 import { CorsOptions } from 'cors';
 
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000'];
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000', 'https://sevi-go-three.vercel.app'];
 
 const corsOptions: CorsOptions = {
   origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
